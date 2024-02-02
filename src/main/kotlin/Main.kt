82b86e126bda336c0fc115316b456502e07594e8
Mohamed Shil <maxamed-14@outlook.dk>
2024-02-02 18:07:49 +0100

    Finished all crud operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import com.mongodb.MongoException
 import com.mongodb.client.model.Filters
+import com.mongodb.client.model.UpdateOptions
+import com.mongodb.client.model.Updates
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
@@ -15,37 +17,24 @@ fun main(args: Array<String>) {
 
     val running:Boolean = true
 
-//    while (running){
-//        println("Enter your name: ")
-//        val name = readln()
-//        println("Enter your age: ")
-//        val age = readln().toInt()
-//        println("Enter your degree: ")
-//        val degree = readln()
-//        println("Enter your gender")
-//        val gender = readln()
-//        println("Enter your occupation: ")
-//        val occupation = readln()
-//        println("Enter your first skill")
-//        val stack1 = readln()
-//        println("Enter your second skill")
-//        val stack2 = readln()
-//        println("Enter your third skill")
-//        val stack3 = readln()
-//
-//        val stackList:MutableList<String> = mutableListOf(stack1,stack2,stack3)
-
+    while (running)
+    {
         try {
             runBlocking {
 
-                println("Press 1 to read and 2 to delete")
+                println("Press 1 to add, 2 to read, 3 to update or 4 to delete a person")
                 val choice = readln().toInt()
 
                 when(choice){
-                    1 -> readPerson(collection)
-                    2-> deletePerson(collection)
+                    1 -> addPerson(collection)
+                    2 -> readPerson(collection)
+                    3 -> updatePerson(collection)
+                    4 -> deletePerson(collection)
+
+                    else -> {
+                        throw IllegalArgumentException("Invalid integer. Enter a number from 1-4")
+                    }
                 }
-//            addPerson(database, name, age, degree, gender, occupation, stackList)
             }
         }
 
@@ -56,10 +45,10 @@ fun main(args: Array<String>) {
         catch (exception: Exception) {
             print(exception.message)
         }
+    }
 
         client.close()
     }
-//}
 
 fun getDatabase(): MongoDatabase {
 
@@ -79,9 +68,27 @@ data class Person(
 )
 
 suspend fun addPerson(
-    database: MongoDatabase, name: String, age: Int, degree: String,
-    gender: String, occupation: String, stack: MutableList<String>
+    collection: MongoCollection<Person>
 ) {
+    println("Enter your name: ")
+    val name = readln()
+    println("Enter your age: ")
+    val age = readln().toInt()
+    println("Enter your degree: ")
+    val degree = readln()
+    println("Enter your gender")
+    val gender = readln()
+    println("Enter your occupation: ")
+    val occupation = readln()
+    println("Enter your first skill")
+    val stack1 = readln()
+    println("Enter your second skill")
+    val stack2 = readln()
+    println("Enter your third skill")
+    val stack3 = readln()
+
+    val stackList:MutableList<String> = mutableListOf(stack1,stack2,stack3)
+
     val info =
         Person(
             id = ObjectId(),
@@ -90,12 +97,11 @@ suspend fun addPerson(
             degree = degree,
             gender = gender,
             occupation = occupation,
-            stack = stack
+            stack = stackList
         )
 
-    val collection = database.getCollection<Person>("Person")
     collection.insertOne(info).also {
-        println("Inserted id: ${it.insertedId}")
+        println("Inserted id: ${it.insertedId} $name into the collection")
     }
 }
 
@@ -104,18 +110,39 @@ suspend fun readPerson(collection: MongoCollection<Person>){
 
     val query = Filters.or(
         listOf(
-            Filters.eq(Person::name.name, "Mohamed")
+            Filters.eq(Person::name.name, "Mohamed"),
+            Filters.eq(Person::gender.name,"Female")
         )
     )
 
     collection.find(filter = query).collect{
         person ->
-        print("Found person with name: ${person.name}, degree in: ${person.degree} and works as an ${person.occupation}")
+        print("Found person with name: ${person.name}, degree in: ${person.degree} and works as an ${person.occupation} The second skill is: ${person.stack[1]}")
+    }
+}
+
+suspend fun updatePerson(collection: MongoCollection<Person>){
+
+    val query = Filters.eq(Person::occupation.name, "Astrophycisist @ NASA")
+    val update = Updates.combine(
+
+        Updates.set(Person::name.name, "Neil Degrasse Tyson"),
+        Updates.set(Person::degree.name, "PhD. Astrophysics"),
+        Updates.set(Person::age.name, 65)
+    )
+
+    val updateOptions = UpdateOptions().upsert(true)
+
+    collection.updateMany(filter = query,update,updateOptions).also {
+        println("Updated ${it.modifiedCount} documents in the collection")
     }
 }
 
 suspend fun deletePerson(collection: MongoCollection<Person>) {
-    val query = Filters.eq(Person::name.name, "")
+
+    println("Enter the name of the person you want to delete")
+    val personToDelete = readln()
+    val query = Filters.eq(Person::name.name, personToDelete)
 
     if (query.equals(0)) {
        println("No document found with the given query!")
@@ -124,7 +151,7 @@ suspend fun deletePerson(collection: MongoCollection<Person>) {
     else
     {
         collection.deleteMany(filter = query).also {
-            println("Deleted ${it.deletedCount} documents from the database")
+            println("Deleted ${it.deletedCount} documents from the collection")
         }
     }
 }
